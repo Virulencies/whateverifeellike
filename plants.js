ea2d9663fa3ac661297bb7086f75ea7cf1f5e4c7
Theo McCray <tlmccraycray@gmail.com>
2023-06-22 10:28:52 -0600

    Add if statement

--- plants.js
+++ plants.js
@@ -1,7 +1,9 @@
-var gets_sunshine = true
+var gets_sunshine = false
 var ounces_of_water = 9
 if (gets_sunshine === true && ounces_of_water >= 10){
     console.log("the plant will thrive")
+} else if (gets_sunshine === true || ounces_of_water >= 10) {
+    console.log("the plant will be fine")
 } else {
    console.log("the plant will die") 
 }
