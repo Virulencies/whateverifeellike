b39aa6b835995f344dffec23b9fe4da1e651cdaa
Theo McCray <tlmccraycray@gmail.com>
2023-06-22 10:24:09 -0600

    Initial commit

---
+++ plants.js
@@ -0,0 +1,7 @@
+var gets_sunshine = true
+var ounces_of_water = 9
+if (gets_sunshine === true && ounces_of_water >= 10){
+    console.log("the plant will thrive")
+} else {
+   console.log("the plant will die") 
+}
